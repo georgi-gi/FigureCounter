7c4e6209bc9f57113f27e5533e3adcd004060eb7
georgi-gi <georgi.g.ivanov.95@gmail.com>
2018-09-15 22:22:55 +0300

    Add oneLine and oneCol tests

--- FigureCounterTests/test.cpp
+++ FigureCounterTests/test.cpp
@@ -44,11 +44,59 @@ namespace {
 	{
 		std::vector<std::vector<bool>> testMatrix =
 		{
-			{ true, true},
-			{ true, true}
+			{ true, true, true, true, true},
+			{ true, true, true, true, true}
 		};
 
 		EXPECT_EQ(countFigures(testMatrix), 1);
+	}
+
+	TEST(CountFiguresTest, OneLineAllMarked)
+	{
+		std::vector<std::vector<bool>> testMatrix =
+		{
+			{ true, true, true, true, true }
+		};
+
+		EXPECT_EQ(countFigures(testMatrix), 1);
+	}
+
+	TEST(CountFiguresTest, OneLineNoMarked)
+	{
+		std::vector<std::vector<bool>> testMatrix =
+		{
+			{ false, false, false, false, false }
+		};
+
+		EXPECT_EQ(countFigures(testMatrix), 0);
+	}
+
+	TEST(CountFiguresTest, OneColAllMarked)
+	{
+		std::vector<std::vector<bool>> testMatrix =
+		{
+			{ true },
+			{ true },
+			{ true },
+			{ true },
+			{ true }
+		};
+
+		EXPECT_EQ(countFigures(testMatrix), 1);
+	}
+
+	TEST(CountFiguresTest, OneColNoMarked)
+	{
+		std::vector<std::vector<bool>> testMatrix =
+		{
+			{ false },
+			{ false },
+			{ false },
+			{ false },
+			{ false }
+		};
+
+		EXPECT_EQ(countFigures(testMatrix), 0);
 	}
 }
 
