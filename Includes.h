7eb50fcc0481d50ac360a0d613ca09085b026837
georgi-gi <georgi.g.ivanov.95@gmail.com>
2018-09-16 00:25:44 +0300

    Add consts

--- Includes.h
+++ Includes.h
@@ -3,15 +3,19 @@
 #include <queue>
 #include "MatrixElement.h"
 
-std::vector<MatrixElement> generateNeighbours(MatrixElement& currentElement, size_t rows, size_t cols)
+std::vector<MatrixElement> generateNeighbours(const MatrixElement& currentElement, const size_t rows, const size_t cols)
 {
 	std::vector<MatrixElement> neighbours;
+	// left neighbour
 	if (currentElement.col > 0)
 		neighbours.push_back(MatrixElement(currentElement.row, currentElement.col - 1));
+	// right neighbour
 	if (currentElement.col < cols - 1)
 		neighbours.push_back(MatrixElement(currentElement.row, currentElement.col + 1));
+	// upper neighbour
 	if (currentElement.row > 0)
 		neighbours.push_back(MatrixElement(currentElement.row - 1, currentElement.col));
+	// lower neighbour
 	if (currentElement.row < rows - 1)
 		neighbours.push_back(MatrixElement(currentElement.row + 1, currentElement.col));
 
@@ -22,15 +26,15 @@ void lookAround(const MatrixElement& startingElement,
 				const std::vector<std::vector<bool>>& matrix,
 				std::vector<std::vector<bool>>& isVisited)
 {
-	size_t cntRows = matrix.size();
-	size_t cntCols = matrix[0].size();
+	const size_t cntRows = matrix.size();
+	const size_t cntCols = matrix[0].size();
 
 	std::queue<MatrixElement> elementsQueue;
 	elementsQueue.push(startingElement);
 
 	while (!elementsQueue.empty())
 	{
-		MatrixElement& currentElement = elementsQueue.front();
+		const MatrixElement& currentElement = elementsQueue.front();
 
 		std::vector<MatrixElement> neighbours = generateNeighbours(currentElement, cntRows, cntCols);
 
